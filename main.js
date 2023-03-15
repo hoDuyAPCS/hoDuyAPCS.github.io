afd1a34e71c709ad1193308378787b8555e8bcf9
Ho Duy <40297016+hoDuyAPCS@users.noreply.github.com>
2023-03-15 14:18:03 +0700

    minor fix to track scroll

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const handleOnMove = e => {
   
   const percentage = (mouseDelta / maxDelta) * -100,
         nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -50);
   
   track.dataset.percentage = nextPercentage;
   
